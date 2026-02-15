604eef57eb543968ceb6513638ed4c063410b3fe
quinnc0 <quinnc04@gmail.com>
2026-02-14 18:44:18 -0700

    Change sleep time to 20 min. #4

--- include/config.h
+++ include/config.h
@@ -58,7 +58,7 @@
 // ============================================================================
 
 #define SLEEP_DELAY_MS 5000         // Sleep after 5 seconds in loop
-#define TIMER_WAKEUP_SECONDS 300    // Wake up every 5 minutes for battery check (only used if BATTERY_ENABLED)
+#define TIMER_WAKEUP_SECONDS 12000    // Wake up every X seconds for battery check (only used if BATTERY_ENABLED)
 
 // ============================================================================
 // Serial Configuration
